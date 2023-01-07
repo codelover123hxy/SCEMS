8f56ca7bcfbcd2ec33fc778b13cc9a851480eed5
codelover123hxy <2687057458@qq.com>
2023-01-07 18:58:21 +0800

    renew files

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,11 +5,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    identity:'',   // 标注学生或辅导员
+    username:'',
+    password:'',
+    student:{
+      grade:'',
+      profession:''
+    }
   },
 
   login(){
+    let username=this.data.username;
+    let pwd=this.data.password;
+    let identity=this.data.identity;
+  
+    if (identity==''){
+      wx.showToast({
+        title: '请选择身份',
+        icon:'error'
+      })
+    }
+    else if(identity=="学生") {
+      let obj={};
 
+      obj.grade=username.substring(0,4);
+      console.log(obj.grade);
+      obj.profession="计算机";
+      this.setData({
+        student:obj
+      })
+      wx.request({
+        url: 'url',
+        method:'POST',
+        success:(res)=>{
+
+        },
+        complete:()=>{
+          wx.navigateBack({
+            delta: 0,
+          })
+        }
+      })
+    }
+    else if(identity=="辅导员"){
+      wx.request({
+        url: 'url',
+        method:'POST',
+        success:(res)=>{
+
+        },
+        complete:()=>{
+          wx.navigateBack({
+            delta: 0,
+          })
+        }
+      })
+    }
+  },
+//  双向绑定
+  inputUsername(e){
+    var name=e.detail.value;
+    this.setData({
+      username:name
+    })
+  },
+
+  inputPwd(e){
+    var pwd=e.detail.value;
+    this.setData({
+      password:pwd
+    })
+  },
+
+  radioChange(e){
+    var identity=e.detail.value;
+    this.setData({
+      identity:identity
+    })
   },
   /**
    * 生命周期函数--监听页面加载
